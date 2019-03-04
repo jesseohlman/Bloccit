8f72c0695616d62903461de949678db61151686b
jesseohlman <44782018+jesseohlman@users.noreply.github.com>
2019-03-04 11:50:53 -0500

    WIP deploy production

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const routeConfig = require("./config/route-config");
+const appConfig = require("./config/main-config");
 const app = express();
 
 const mainRoute = require("./routes/static");
 //const routeConfig = require("./config/route-config.js");
 
+appConfig.init();
 //routeConfig.init(app);
 
 app.use('/', mainRoute);
