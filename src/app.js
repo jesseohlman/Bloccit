d389c63d6aa8b60082ed7398d37638d772c99d60
jesseohlman <44782018+jesseohlman@users.noreply.github.com>
2019-03-04 17:36:45 -0500

    added views and optimized controls

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const routeConfig = require("./config/route-config");
+//const routeConfig = require("./config/route-config");
 const appConfig = require("./config/main-config");
 const app = express();
 
 const mainRoute = require("./routes/static");
-//const routeConfig = require("./config/route-config.js");
+const routeConfig = require("./config/route-config.js");
 
-appConfig.init();
-//routeConfig.init(app);
+appConfig.init(app, express);
+routeConfig.init(app);
 
 app.use('/', mainRoute);
 
--- src/marco.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/marco", (req, res, next) => {
-    res.send("polo");
-});
-
-module.exports = app;
