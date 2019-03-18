dc2857e0a1d9b57476d90405c6af5b48f7acdea6
jesseohlman <44782018+jesseohlman@users.noreply.github.com>
2019-03-18 15:51:42 -0400

    added sign-in and sign-out functionality

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -5,7 +5,9 @@ module.exports = {
         const topicRoutes = require("../routes/topics");
         const postRoutes = require("../routes/posts");
         const flairRoutes = require("../routes/flairs");
+        const userRoutes = require("../routes/users");
 
+        app.use(userRoutes);
         app.use(flairRoutes);
         app.use(postRoutes);
         app.use(staticRoutes);
