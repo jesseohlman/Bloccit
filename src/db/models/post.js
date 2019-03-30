0470a05bd85eefb3a28ea6e3e81e45c8eb2e70bb
jesseohlman <44782018+jesseohlman@users.noreply.github.com>
2019-03-30 15:51:03 -0400

    added the favoriting

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -45,6 +45,11 @@ module.exports = (sequelize, DataTypes) => {
       as: "votes"
     });
 
+    Post.hasMany(models.Favorite, {
+      foreignKey: "postId",
+      as: "favorites"
+    });
+
     Post.prototype.getPoints = function(){
       if(this.votes.length === 0) return 0;
 
@@ -53,6 +58,18 @@ module.exports = (sequelize, DataTypes) => {
         .reduce((prev, next) => { return prev + next });
     };
 
+    Post.prototype.getFavoriteFor = function(userId){
+      return this.favorites.find((favorite) => {
+        return favorite.userId == userId
+      });
+    }
+
+    Post.afterCreate((post, callback) => {
+      return models.Favorite.create({
+        userId: post.userId,
+        postId: post.id
+      });
+    });
   };
 
   return Post;
